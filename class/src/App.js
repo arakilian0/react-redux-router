2fca4a77168c9a7f2422b35d66d086159bd5c6b7
Michael Arakilian <arakilian0@gmail.com>
2021-08-01 11:54:58 -0700

    Add ref to react app on App.js

--- class/src/App.js
+++ class/src/App.js
@@ -4,6 +4,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
+        <p>CLASS</p>
         <h1>Hello World</h1>
       </div>
     )
--- function/src/App.js
+++ function/src/App.js
@@ -1,7 +1,8 @@
 export default function App() {
   return (
     <div>
-      <h1>Hello Worlds</h1>
+      <p>FUNCTION</p>
+      <h1>Hello World</h1>
     </div>
   )
 }
